6ccc50e89a4eb86b03955c4911177a7369e1da4c
Daniel Rojo <danielrojo@gmail.com>
2022-03-23 15:47:46 +0100

    heroes component modified

--- src/components/heroes/HeroesComponent.vue
+++ src/components/heroes/HeroesComponent.vue
@@ -9,17 +9,34 @@
         class="form-control"
         placeholder=""
         aria-describedby="helpId"
-        v-model="heroName"
+        v-model="newHero.name"
+      />
+      <label for="">Hero Alias</label>
+      <input
+        type="text"
+        name=""
+        id=""
+        class="form-control"
+        placeholder=""
+        aria-describedby="helpId"
+        v-model="newHero.alias"
       />
     </div>
-    <button type="button" class="btn btn-primary" @click="addHero()" :disabled="heroName === ''">
+    <button
+      type="button"
+      class="btn btn-primary"
+      @click="addHero()"
+      :disabled="newHero.name === ''"
+    >
       Add Hero
     </button>
     <hr />
 
     <ul class="list-group">
       <li class="list-group-item" v-for="(hero, index) in heroes" :key="index">
-        {{ hero }}
+        <b>{{ hero.name }}</b>
+        <br />
+        {{ hero.alias }}
       </li>
     </ul>
   </div>
@@ -27,19 +44,27 @@
 
 <script lang="ts">
 import Vue from "vue";
+import Hero from "../../models/Hero";
 
 export default Vue.extend({
   name: "HeroesComponent",
   data() {
     return {
-      heroes: ["Batman", "Superman", "Spiderman"],
-      heroName: "",
+      heroes: [
+        new Hero("Batman", "Bruce Wayne"),
+        new Hero("Superman", "Clark Kent"),
+        new Hero("Spiderman", "Peter Parker"),
+      ],
+      newHero: {
+        name: "",
+        alias: "",
+      },
     };
   },
   methods: {
     addHero() {
-      this.heroes.push(this.heroName);
-      this.heroName = "";
+      this.heroes.push(this.newHero);
+      this.newHero = new Hero();
     },
   },
 });
---
+++ src/models/Hero.ts
@@ -0,0 +1,3 @@
+export default class Hero {
+    constructor(public name = '', public alias = '') {}
+}
