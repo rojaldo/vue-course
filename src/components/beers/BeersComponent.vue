85915b585f785dda1e798d947ab469f699301842
Daniel Rojo <danielrojo@gmail.com>
2022-03-28 16:26:56 +0200

    beers range done

--- src/components/beers/BeersComponent.vue
+++ src/components/beers/BeersComponent.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
+      <vue-slider v-model="range"></vue-slider>
     <div class="row">
-      <div class="col-xs-12 col-md-6 col-lg-4 col-xl-3 mb-3" v-for="(beer, index) in beers" :key="index">
+      <div class="col-xs-12 col-md-6 col-lg-4 col-xl-3 mb-3" v-for="(beer, index) in beersInRange" :key="index">
         <div class="card h-100">
           <img class="card-img-top mx-auto mt-2" :src="beer.imageUrl" :alt="beer.name" style="width: 60px"/>
           <div class="card-body">
@@ -18,13 +19,22 @@
 <script lang="ts">
 import Vue from "vue";
 import Beer from "../../models/Beer";
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
 
 export default Vue.extend({
   name: "BeersComponent",
+  components: { VueSlider },
   data() {
     return {
       beers: new Array<Beer>(),
+        range: [0,5],
     };
+  },
+  computed: {
+    beersInRange(): Array<Beer> {
+      return this.beers.filter(beer => beer.abv >= this.range[0] && beer.abv <= this.range[1]).sort((a, b) => a.abv - b.abv);
+    }
   },
   mounted() {
     this.getBeers();
